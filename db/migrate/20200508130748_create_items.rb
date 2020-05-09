083097ebb71aa09a4032a4a3a29c818a96d1a316
keisuke-kk <candy0220.k@gmail.com>
2020-05-09 14:51:23 +0900

    create purchase's check

---
+++ db/migrate/20200508130748_create_items.rb
@@ -0,0 +1,12 @@
+class CreateItems < ActiveRecord::Migration[5.2]
+  def change
+    create_table :items do |t|
+      t.string    :item_name, null: false
+      t.integer   :price, null: false
+      t.string    :size, null: false
+      t.text      :content, null: false
+      t.integer   :status, null: false
+      t.timestamps
+    end
+  end
+end
