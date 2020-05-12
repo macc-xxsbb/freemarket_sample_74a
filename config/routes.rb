21eb4d8615eba6ca63b5b5242b9b5c51710d5194
shontanakamura <63178332+shontanakamura@users.noreply.github.com>
2020-05-12 22:11:39 +0900

    routes fix

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,9 @@ Rails.application.routes.draw do
   root 'items#index'
 
 
-  resources :items, only: [:index, :show] do
+  resources :items, only: [:index, :show, :new] do
     resources :comment, only: :create
   end
-
-  resources :items, only: [:index, :show, :new]
   resources :item_payment
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
