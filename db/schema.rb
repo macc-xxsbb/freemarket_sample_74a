4f1b062f543008336152e9d2f8fb75068254af55
shota-uchiyama <s.uchiyama25@gmail.com>
2020-05-14 17:19:15 +0900

    cccc

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,12 @@
 # It's strongly recommended that you check this file into your version control system.
 
 
+
 ActiveRecord::Schema.define(version: 2020_05_12_074314) do
 
 ActiveRecord::Schema.define(version: 2020_05_12_084409) do
+
+
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "prefecture_id"
