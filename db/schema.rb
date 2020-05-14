b9924d0c84d4a6db4796f95f8cd08edefecb7efd
macc-xxsbb <nexus41231@gmail.com>
2020-05-14 16:31:34 +0900

    ok

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_14_061128) do
-
-  create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer  "zipcode"
-    t.text     "address"
-    t.bigint   "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index   ["user_id"], name: "index_addresses_on_user_id"
-
-ActiveRecord::Schema.define(version: 2020_05_12_074314) do
-
 ActiveRecord::Schema.define(version: 2020_05_12_084409) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
