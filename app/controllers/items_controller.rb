d63fb1caea2af47ee75cac1baa70ff17d00ab087
shontanakamura <63178332+shontanakamura@users.noreply.github.com>
2020-05-14 16:23:09 +0900

    comment

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,13 +1,16 @@
 class ItemsController < ApplicationController
   def index
-
+    @item = Item.all
   end
-  
+
   def show
+    # @item = Item.find(params[:id])
+    @categories = Category.select("title")
+    # @comment = comment.new
+    
   end
-  
-  def new
 
+  def new
   end
 
 end
