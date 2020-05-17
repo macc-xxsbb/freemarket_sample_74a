b60215e0b51d4c2b8b1db0021e638b704306f155
macc-xxsbb <nexus41231@gmail.com>
2020-05-17 22:27:54 +0900

    ok

--- db/schema.rb
+++ db/schema.rb
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2020_05_17_082024) do
     t.string "brand", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "item_id"
   end
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
