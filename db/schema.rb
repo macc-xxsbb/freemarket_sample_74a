efb1483168c4fdc4dacbfa54a53926ab937d0bed
carmani-san <63335109+carmani-san@users.noreply.github.com>
2020-05-21 21:03:26 +0900

    okbaby5

--- db/schema.rb
+++ db/schema.rb
@@ -32,7 +32,7 @@ ActiveRecord::Schema.define(version: 2020_05_21_032622) do
   end
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "title"
+    t.string "title", null: false
     t.string "ancestry"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -74,7 +74,7 @@ ActiveRecord::Schema.define(version: 2020_05_21_032622) do
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "item_name", null: false
     t.integer "price", null: false
-    t.string "size"
+    t.string "size", null: false
     t.text "content", null: false
     t.integer "status", null: false
     t.datetime "created_at", null: false
