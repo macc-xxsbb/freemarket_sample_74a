861ccf30a8cbd15390703ae8fd4fb8ce86d9aacf
macc-xxsbb <nexus41231@gmail.com>
2020-05-26 08:45:45 +0900

    おk

--- app/models/credit_card.rb
+++ app/models/credit_card.rb
@@ -1,9 +1,4 @@
 class CreditCard < ApplicationRecord
   belongs_to :user
-
-  validates :card_number, :card_year, :card_month, :security_cord, presence: true
-  validates :card_number, :security_code, numericality: { only_integer: true }
-  validates :card_number, length: { is: 16 }
-  validates :security_code, length: { in: 3..4 }
-
+  validates  :customer_id, :card_id, presence: true
 end
