8e4250a43586beaf2797a91be7817115c534299f
carmani-san <63335109+carmani-san@users.noreply.github.com>
2020-05-25 21:17:54 +0900

    ok

---
+++ spec/factories/credit_cards.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+
+  factory :credit_card do
+    card_number   {"1111111111111111"}
+    card_year     {"25"}
+    card_month    {"1"}
+    security_code {"111"}
+  end
+
+end
