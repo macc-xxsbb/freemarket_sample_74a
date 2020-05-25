b16ee70f6d7d594400866cc4bebceca08e226590
macc-xxsbb <nexus41231@gmail.com>
2020-05-25 20:20:48 +0900

    yess

--- app/models/shipping.rb
+++ app/models/shipping.rb
@@ -1,9 +1,6 @@
 class Shipping < ApplicationRecord
 
   belongs_to :item, optional: true
-  validates :ship_base, presence: true
-  validates :region, presence: true
-  validates :ship_date, presence: true
   validates :prefecture_id, presence: true
   
 end
