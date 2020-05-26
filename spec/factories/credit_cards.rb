91607e8eba1d6da5294cf1bd5f8bbcd81f4efba0
macc-xxsbb <nexus41231@gmail.com>
2020-05-26 11:23:42 +0900

    おk

--- spec/factories/credit_cards.rb
+++ spec/factories/credit_cards.rb
@@ -1,10 +1,8 @@
 FactoryBot.define do
 
   factory :credit_card do
-    card_number   {"1111111111111111"}
-    card_year     {"25"}
-    card_month    {"1"}
-    security_code {"111"}
+    customer_id   {"cus_cf6e7e7da18475fd5b1c0de7ed67"}
+    card_id       {"car_54e71df8f4b5a5d1c746cf940a34"}
   end
 
 end
